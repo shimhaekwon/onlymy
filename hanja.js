75fd6769fb301d3c96035a3af30b1ed6d76c255b
shimhaekwon <89654237+shimhaekwon@users.noreply.github.com>
2023-05-19 10:18:54 +0900

    Add files via upload

---
+++ hanja.js
@@ -0,0 +1,12 @@
+const funcGetJsonData = function(fileName){
+    const retArr = [];
+    fetch(fileName)
+        .then(response => response.json())
+        .then(data => {
+            retArr = data.map(item => item.property);
+        })
+        .catch(error => {
+            console.error('Error parsing JSON:', error);
+        });
+    return retArr;
+};
